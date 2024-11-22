9393bf1f82c574be8650c058bf3f0894a636c9d2
Salvatore Cuccurullo <sa.cuccurullo@douglas.de>
2024-11-22 16:46:32 +0100

    adding sleep 30

--- 00-install-base-software.sh
+++ 00-install-base-software.sh
@@ -3,6 +3,8 @@
 echo "Installing the Nginx controller..."
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.11.3/deploy/static/provider/baremetal/deploy.yaml
 
+sleep 30
+
 echo "Creating Load Balancer service"
 kubectl -n ingress-nginx create -f loadbalabcer.yaml
 
