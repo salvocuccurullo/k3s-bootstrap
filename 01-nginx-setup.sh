9d5175ef64f10d475c3f9679af808d05f6a25d94
Salvatore Cuccurullo <sa.cuccurullo@douglas.de>
2024-11-22 16:14:06 +0100

    fix namespaces on some yaml files

--- 01-nginx-setup.sh
+++ 01-nginx-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing the Nginx controller..."
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/baremetal/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.11.3/deploy/static/provider/baremetal/deploy.yaml
 
 echo "Creating Load Balancer service"
-kubectl create -f loadbalabcer.yaml
+kubectl -n ingress-nginx create -f loadbalabcer.yaml
